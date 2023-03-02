afd9192861c256330081baa3ca154e5e86d5844e
Axel Hanikel <ahanikel@adobe.com>
2023-03-02 09:41:02 +0100

    WIP header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,4 @@
-import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
+import { getMetadata, decorateIcons, decorateButtons } from '../../scripts/lib-franklin.js';
 
 /**
  * decorates the header
@@ -34,8 +34,53 @@ export default async function decorate(block) {
 
     logoPicture.querySelector('img').setAttribute('alt', logoAltText);
     menuRight.classList.add('top-menu-right');
+    const m1 = menuRight.querySelector('ul');
+    const m2 = m1.querySelector('ul');
+    const p = document.createElement('p');
+    const a = document.createElement('a');
+    a.appendChild(m2);
+    p.appendChild(a);
+    m1.remove(m2);
+    m1.appendChild(p);
 
-    block.appendChild(logo);
-    block.appendChild(menuRight);
+    for (let li of m2.querySelectorAll('li')) {
+        const text = li.childNodes[0];
+        const a = document.createElement('a');
+        li.removeChild(text);
+        a.appendChild(text);
+        a.setAttribute('href', '#');
+        li.appendChild(a);
+    }
+
+    const leftItems = document.createElement('div');
+    leftItems.classList.add('align-left');
+    leftItems.appendChild(logo);
+
+    const breadCrumb = document.createElement('p');
+    breadCrumb.classList.add('breadcrumb');
+    for (let item of ['bread', 'crumb', 'path']) {
+        const p = document.createElement('span');
+        p.textContent = item;
+        breadCrumb.appendChild(p);
+    }
+    leftItems.appendChild(breadCrumb);
+
+    const rightItem = document.createElement('nav');
+    const headerMenuToggle = document.createElement('input');
+    headerMenuToggle.setAttribute('type', 'checkbox');
+    headerMenuToggle.id = 'header-menu-toggle';
+
+    const headerMenuIcon = document.createElement('label');
+    headerMenuIcon.setAttribute('for', headerMenuToggle.id);
+    headerMenuIcon.classList.add('header-menu-icon');
+    headerMenuIcon.textContent = '☰';
+    m2.classList.add('header-menu-right');
+
+    rightItem.appendChild(headerMenuToggle);
+    rightItem.appendChild(m2);
+    rightItem.appendChild(headerMenuIcon);
+    block.appendChild(leftItems);
+    block.appendChild(rightItem);
+    decorateButtons(m1);
   }
 }
